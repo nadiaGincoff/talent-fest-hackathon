ec7176b4e7ca654b4b165dff37ccc4e513b2886e
natalieechenique <natalie.terapias@gmail.com>
2020-09-28 20:07:57 -0300

    merging master develop

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -5,46 +5,34 @@ import React from "react";
 // import {Link} from 'react-router-dom'
 // import logo from '../Images/EY_Logo.png';
 
-const Toolbar = (props) => {
-    // const { handleLogout } = useContext(UserDataContext);
+const toolbar = props => (
+    <header className='toolbar'>
+        <nav className='toolbar__navigation'>
+        <div className='toolbar__toggle-button'>
+           <DrawerToggleButton click={props.drawerClickHandler} />
+           </div> 
 
-    return(
-        <header className="toolbar">
-            <nav className="toolbar__navigation">
-                <div className="toolbar__toggle-button">
-                  <DrawerToggleButton click={props.drawerClickHandler} />
-                </div>
+<div className='toolbar__logo'>
+<a href='/'>EY</a>
+</div>
 
-                <div className="toolbar__logo">
-                    <a href="/">EY</a>
-                </div>
+{/* <a href='/'><img src={logo} alt="Logo EY" width='100px'/></a> */}
 
-            {/* <a href='/'><img src={logo} alt="Logo EY" width='100px'/></a> */}
-        
-                <div className="spacer" />
-                <div className="toolbar_navigation-items">
-                    <ul>
-                      <li>
-                        <a href="/Notifications">Notificaciones</a>
-                      </li>
-                      <li>
-                        <a href="/Messages">Mensajes</a>
-                      </li>
-                      <li>
-                        <a href="/User">Usuario</a>
-                      </li>
-                      <li>
 
-                        <a href="/Login">
-                          Salir
-                        </a>
+<div className='spacer' />
+<div className='toolbar_navigation-items'>
+ <ul>
 
-                      </li>
-                    </ul>
-                </div>
-          </nav>
-        </header>
-    )
-};
+ <li><a href='/Login'>Entrar</a></li>
+    <li><a href='/Notifications'>Notificaciones</a></li>
+    <li><a href='/Messages'>Mensajes</a></li>
+    <li><a href='/User'>Usuario</a></li>
+    <li><a href='/Exit'>Salir</a></li>
+ </ul>
+    </div>  
+        </nav>
+    </header>
+);
 
-export default Toolbar;
+export default toolbar;
+
